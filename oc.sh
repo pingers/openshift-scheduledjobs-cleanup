27b00aa690e61ac91fb8c451a550d44677a90ba1
Michael Priest <pingers@gmail.com>
2018-01-19 10:47:44 +1030

    Use no headers option.

--- oc.sh
+++ oc.sh
@@ -9,8 +9,7 @@ DEFAULT_NAMESPACE=$(eval cat /var/run/secrets/kubernetes.io/serviceaccount/names
 oc login https://$KUBERNETES_PORT_443_TCP_ADDR:$KUBERNETES_SERVICE_PORT_HTTPS \
   --token `cat /var/run/secrets/kubernetes.io/serviceaccount/token` \
   --certificate-authority=/var/run/secrets/kubernetes.io/serviceaccount/ca.crt
-oc get jobs -n ${DEFAULT_NAMESPACE} > /tmp/jobs
-tail -n +2 /tmp/jobs > /tmp/jobs-without-header
+oc get jobs --no-headers=true -n ${DEFAULT_NAMESPACE} > /tmp/jobs-without-header
 
 while read JOB_WITH_STATS
 do
